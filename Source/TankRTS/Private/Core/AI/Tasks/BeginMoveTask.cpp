a8ff86455425dc06d4715986b679751ef8954b2e
Jack Weddell <jacksweddell@outlook.com>
2024-09-21 13:19:37 +0100

    AI tasks - bookend the commanded move to, and toggle the UnitStatus flag

---
+++ Source/TankRTS/Private/Core/AI/Tasks/BeginMoveTask.cpp
@@ -0,0 +1,36 @@
+// Copyright Jack Wedell 2024
+
+#include "Core/AI/Tasks/BeginMoveTask.h"
+#include "AIController.h"
+#include "Core/Units/Base/UnitBase.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Core/AI/UnitAIStatus.h"
+
+UBeginMoveTask::UBeginMoveTask()
+{
+    NodeName = TEXT("Set-off Movement");
+    BlackboardKey.AddBoolFilter(this, GET_MEMBER_NAME_CHECKED(UBeginMoveTask, BlackboardKey));
+}
+
+EBTNodeResult::Type UBeginMoveTask::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+
+    AAIController* AIController { OwnerComp.GetAIOwner() };
+
+    if (AIController) {
+        APawn* AIPawn { AIController->GetPawn() };
+        if (AIPawn) {
+            AUnitBase* Unit = Cast<AUnitBase, APawn>(AIPawn);
+            Unit->SetIsUnitMoving(true);
+
+            AIController->GetBlackboardComponent()->SetValueAsEnum( FName( "UnitStatus" ), UnitAIStatus::MovingTo );
+        }
+    }
+
+    return EBTNodeResult::Type();
+}
+
+FString UBeginMoveTask::GetStaticDescription() const
+{
+    return FString(TEXT("DD"));
+}
---
+++ Source/TankRTS/Private/Core/AI/Tasks/RTSEndMoveTo.cpp
@@ -0,0 +1,26 @@
+// Copyright Jack Wedell 2024
+
+#include "TankRTS/Public/Core/AI/Tasks/RTSEndMoveTo.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Core/AI/UnitAIStatus.h"
+
+URTSEndMoveTo::URTSEndMoveTo()
+{
+    NodeName = TEXT("End Move Task - Switch Enum");
+}
+
+EBTNodeResult::Type URTSEndMoveTo::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+    AAIController* AIController { OwnerComp.GetAIOwner() };
+
+    if (AIController) {
+        AIController->GetBlackboardComponent()->SetValueAsEnum(FName("UnitStatus"), UnitAIStatus::Rest);
+
+        return EBTNodeResult::Succeeded;
+    }
+
+    else {
+        return EBTNodeResult::Failed;
+    }
+}

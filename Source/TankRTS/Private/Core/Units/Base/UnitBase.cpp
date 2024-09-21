3688f0fa74cbb4430d275bde4dc6bd006de4cc50
Jack Weddell <jacksweddell@outlook.com>
2024-09-21 13:20:51 +0100

    Added support for the AI patrolling behaviour

--- Source/TankRTS/Private/Core/Units/Base/UnitBase.cpp
+++ Source/TankRTS/Private/Core/Units/Base/UnitBase.cpp
@@ -14,32 +14,49 @@
 #include "BehaviorTree/BlackboardComponent.h"
 #include "TankRTS/Public/Core/AI/UnitAIControllerBase.h"
 
+
 // Sets default values
 AUnitBase::AUnitBase()
 {
     // Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
     PrimaryActorTick.bCanEverTick = true;
 
+    // set up mesh, and use as root
     Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
     SetRootComponent(Mesh);
     Mesh->bReceivesDecals = false;
 
+    // main collision box
     BoxCollisionComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Collision Capsule"));
     BoxCollisionComponent->SetupAttachment(RootComponent);
 
+    // forward box - mostly as a source point for a line trace
     ForwardCollisionComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Forward Collision Capsule"));
     ForwardCollisionComponent->SetupAttachment(RootComponent);
 
+    // aft version of the above
     AftCollisionComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Aft Collision Capsule"));
     AftCollisionComponent->SetupAttachment(RootComponent);
 
+    // custom navigation component
     MovementComponent = CreateDefaultSubobject<UUnitNavMovementComponent>(TEXT("New Movement"));
     MovementComponent->SetUpdatedComponent(RootComponent);
 
+    // marks the floor when unit selected
     SelectionDecal = CreateDefaultSubobject<UDecalComponent>(TEXT("Selected Decal"));
     SelectionDecal->SetupAttachment(RootComponent);
     SelectionDecal->SetVisibility(false);
-   
+
+    // set the default AI behaviour to be rest
+    AController* ControllerPtr = GetController();
+
+    if (ControllerPtr) {
+        AUnitAIControllerBase* AIController = Cast<AUnitAIControllerBase, AController>(ControllerPtr);
+
+        if (AIController) {
+            AIController->GetBlackboardComponent()->SetValueAsEnum(FName("UnitStatus"), UnitAIStatus::Rest);
+        }
+    }
 }
 
 // Called when the game starts or when spawned
@@ -72,9 +89,21 @@ void AUnitBase::SetDestination_Implementation(FVector Destination)
         if (AIController) {
             GEngine->AddOnScreenDebugMessage(7, 1.0f, FColor::Cyan, TEXT("Asking Unit to Move"));
             AIController->GetBlackboardComponent()->SetValueAsVector(FName("ClickedLocation"), Destination);
+            AIController->GetBlackboardComponent()->SetValueAsEnum(FName("UnitStatus"), UnitAIStatus::MovingTo);
+           
         }
     }
 }
+
+void AUnitBase::SetStatus_Implementation( UnitAIStatus Status )
+    {
+   UnitStatus = Status;
+    }
+
+UnitAIStatus AUnitBase::GetUnitStatus_Implementation()
+   {
+   return UnitStatus;
+   }
 
 UBoxComponent* AUnitBase::GetForwardCollisionBox()
 {

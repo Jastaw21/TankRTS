feb7f8df64fa45d566c1f4cf46f8395e61935e35
Jack Weddell <jacksweddell@outlook.com>
2024-10-03 19:52:40 +0100

    Developing base building class

--- Source/TankRTS/Private/RTSBuilding.cpp
+++ Source/TankRTS/Private/RTSBuilding.cpp
@@ -1,5 +1,37 @@
 // Copyright Jack Wedell 2024
 
-
 #include "RTSBuilding.h"
+#include "Components/CapsuleComponent.h"
 
+ARTSBuilding::ARTSBuilding()
+{
+
+    // Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+
+    FInteractableDetails Details;
+    Details.Interact_Type = InteractableType::Building;
+    Details.GamePlayName = FString(TEXT("Base Building"));
+    InteractableDetails = Details;
+}
+
+void ARTSBuilding::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void ARTSBuilding::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void ARTSBuilding::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+ARTSInteractableBase* ARTSBuilding::SelectRTS_Implementation()
+{
+    SelectionDecal->SetVisibility(true);
+    return this;
+}
--- Source/TankRTS/Public/RTSBuilding.h
+++ Source/TankRTS/Public/RTSBuilding.h
@@ -7,11 +7,32 @@
 #include "RTSBuilding.generated.h"
 
 /**
- * 
+ Base Building class
  */
 UCLASS()
 class TANKRTS_API ARTSBuilding : public ARTSInteractableBase
 {
 	GENERATED_BODY()
+
+
+public:
+	ARTSBuilding();
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick( float DeltaTime ) override;
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent( class UInputComponent* PlayerInputComponent ) override;
+
+
+
+	// interface functions
+	virtual ARTSInteractableBase* SelectRTS_Implementation() override;
+
 	
 };

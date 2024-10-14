f704232e62e33f66c29d5907d827de8cb253747b
Jack Weddell <jacksweddell@outlook.com>
2024-10-14 19:48:22 +0100

    Added new base class for RTSUnits

---
+++ Source/TankRTS/Private/Core/Interactables/Units/RTSUnit.cpp
@@ -0,0 +1,34 @@
+// Copyright Jack Wedell 2024
+
+
+#include "Core/Interactables/Units/RTSUnit.h"
+
+// Sets default values
+ARTSUnit::ARTSUnit()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ARTSUnit::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARTSUnit::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ARTSUnit::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
